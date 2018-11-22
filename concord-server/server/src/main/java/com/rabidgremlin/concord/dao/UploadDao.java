6d18740303cb16cbbf528e17d4eef2ab2a49749a
Campbell Foskin <campbell.foskin@gmail.com>
2018-11-23 09:59:45 +1300

    Added vote for user choice

--- concord-server/server/src/main/java/com/rabidgremlin/concord/dao/UploadDao.java
+++ concord-server/server/src/main/java/com/rabidgremlin/concord/dao/UploadDao.java
@@ -2,6 +2,7 @@ package com.rabidgremlin.concord.dao;
 
 import com.rabidgremlin.concord.api.UnlabelledPhrase;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jdbi.v3.sqlobject.CreateSqlObject;
 import org.jdbi.v3.sqlobject.transaction.Transaction;
 
@@ -15,9 +16,6 @@ public interface UploadDao {
     @CreateSqlObject
     PhrasesDao phrasesDao();
 
-    @CreateSqlObject
-    LabelsDao labelsDao();
-
     @Transaction
     default void uploadUnlabelledPhrases(List<UnlabelledPhrase> unlabelledPhrases) {
 
@@ -27,15 +25,20 @@ public interface UploadDao {
             if (unlabelledPhrase.getText().equals("text")){
                 continue;
             }
-            //labelsDao().upsert(unlabelledPhrase.getPossibleLabel());
 
             String phraseId = DigestUtils.md5Hex(unlabelledPhrase.getText());
 
             // remove any existing votes in case we are reloading data
             votesDao().deleteAllVotesForPhrase(phraseId);
+
+            // User choice counts as one vote
+            if (StringUtils.isNotEmpty(unlabelledPhrase.getPossibleLabel()))
+            {
+                votesDao().upsert(phraseId, unlabelledPhrase.getPossibleLabel(), "User");
+            }
+
             phrasesDao().upsert(phraseId, unlabelledPhrase.getText(), false);
         }
-
     }
 }
 
