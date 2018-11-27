3b1dae2b1fe225566507811f5fd0d66b22c605f9
Campbell Foskin <campbell.foskin@gmail.com>
2018-11-27 15:31:47 +1300

    Revert "Added exception handling for invalid configuration"
    This reverts commit b1e589262dbe439e967c6fb53a9706334d1bc560.

--- concord-server/plugin/src/main/java/com/rabidgremlin/concord/plugin/InvalidConfigPropertiesException.java
+++
@@ -1,8 +0,0 @@
-package com.rabidgremlin.concord.plugin;
-
-public class InvalidConfigPropertiesException extends Exception
-{
-    public InvalidConfigPropertiesException(String errorMessage) {
-        super(errorMessage);
-    }
-}
--- concord-server/plugin/src/main/java/com/rabidgremlin/concord/plugin/UnableToGetSuggestionsException.java
+++ concord-server/plugin/src/main/java/com/rabidgremlin/concord/plugin/UnableToGetSuggestionsException.java
@@ -1,8 +1,4 @@
 package com.rabidgremlin.concord.plugin;
 
-public class UnableToGetSuggestionsException extends Exception
-{
-    public UnableToGetSuggestionsException(String errorMessage) {
-        super(errorMessage);
-    }
+public class UnableToGetSuggestionsException {
 }
