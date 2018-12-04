07eeaab0632fd83b1d36c9af3271519dfb60e3ce
Laurence Tews <Laurence.Tews@airnz.co.nz>
2018-12-05 11:35:25 +1300

    label reducer tests

---
+++ concord-ui/src/reducers/label.test.js
@@ -0,0 +1,59 @@
+import reducer from './label'
+
+describe('label reducer', () => {
+
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual(
+          {
+            loading: true, 
+            error: false
+          }
+        )
+    });
+
+  it('should handle call to get all labels', () => {
+    expect(
+      reducer([], {
+        type: 'CALL_GET_ALL_LABELS'
+      })
+    ).toEqual(
+      {
+        loading: true,
+        error: false
+      }
+    )
+  });
+
+  it('should handle successful call to get all labels', () => {
+    const testLabelData = {
+        label : 'Hello',
+        id : 123
+    }
+    expect(
+      reducer([], {
+        type: 'CALL_GET_ALL_LABELS_SUCCEEDED',
+        labelData: testLabelData
+      })
+    ).toEqual(
+      {
+        loading: false,
+        error: false, 
+        labelData: testLabelData
+      }
+    )
+  });
+
+  it('should handle unsuccessful call to get all labels', () => {
+    expect(
+      reducer([], {
+        type: 'CALL_GET_ALL_LABELS_FAILED'
+      })
+    ).toEqual(
+      {
+        loading: false, 
+        error: true
+      }
+    )
+  });
+
+});
