a1c42d9f36b5e577e79471afc020bf1e4382941b
Laurence Tews <Laurence.Tews@airnz.co.nz>
2018-12-05 09:12:59 +1300

    searchbar and login components tests

---
+++ concord-ui/src/login.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Login } from './login';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.2';
+import { Grid  } from 'rmwc/Grid';
+import { Card} from 'rmwc/Card';
+import { Button } from 'rmwc/Button';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+
+describe('login', () => {
+
+    const login = shallow(<Login />);
+
+    it('renders without crashing', () => {
+        console.log(login.debug());
+        expect(login.find(Login));
+        expect(login.find(Grid).length).toBe(1);
+        expect(login.find(Grid).containsMatchingElement(Card));
+        expect(login.find(Card).containsMatchingElement(Button));
+
+    });
+
+    it('contains the correct starting state', () => {
+        expect(login.state('email')).toEqual("");
+        expect(login.state('password')).toEqual("");
+        expect(login.state('invalidData')).toEqual(true);
+    });
+
+});
--- concord-ui/src/searchbar.test.js
+++ concord-ui/src/searchbar.test.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Searchbar } from './searchbar';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16.2';
-
+import { Select } from '@rmwc/select';
+import { Button } from'@rmwc/button';
 
 Enzyme.configure({ adapter: new Adapter() })
 
 function setup() {
     const props = {
-        makeVote: jest.fn(),
         dispatch: jest.fn()
     }
-    const enzymeWrapper = shallow(<Searchbar {...props} />)
-
+    const searchbar = shallow(<Searchbar {...props} />)
+    console.log(searchbar.debug());
     return {
         props,
-        enzymeWrapper
+        searchbar
     }
 }
 
+describe('Searchbar', () => {
 
-it('renders without crashing', () => {
-  const wrapper = shallow(< Searchbar />);
-  const { enzymeWrapper } = setup()
-  expect(enzymeWrapper.find(Searchbar));
+    it('renders without crashing', () => {
+        const { searchbar, props } = setup();
+        expect(searchbar.find(Searchbar));
+        expect(searchbar.find(Searchbar).containsMatchingElement(Select));
+        expect(searchbar.find(Searchbar).containsMatchingElement(Button));
+    });
 });
