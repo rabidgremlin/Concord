4a2ab309118797f370fc5d3ab541499e1b68e9ac
Laurence Tews <Laurence.Tews@airnz.co.nz>
2018-12-05 11:41:03 +1300

    searchbar component unit tests

---
+++ concord-ui/src/searchbar.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Searchbar } from './searchbar';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16.2';
+import { Select } from '@rmwc/select';
+import { Button } from'@rmwc/button';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+function setup() {
+    const props = {
+        dispatch: jest.fn()
+    }
+    const searchbar = shallow(<Searchbar {...props} />)
+    console.log(searchbar.debug());
+    return {
+        props,
+        searchbar
+    }
+}
+
+describe('Searchbar', () => {
+
+    it('renders without crashing', () => {
+        const { searchbar, props } = setup();
+        expect(searchbar.find(Searchbar));
+        expect(searchbar.find(Searchbar).containsMatchingElement(Select));
+        expect(searchbar.find(Searchbar).containsMatchingElement(Button));
+    });
+});
