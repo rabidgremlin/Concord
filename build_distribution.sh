171439b5f7b9b457008942520e39d3df768ca4a7
ackej1 <jonathan.ackerman1@airnz.co.nz>
2018-12-11 14:07:52 +1300

    Tweaked build script.

--- build_distribution.sh
+++ build_distribution.sh
@@ -2,17 +2,17 @@
 
 # Build UI
 cd concord-ui
-npm install
-npm run build
+npm install || exit 1
+npm run build || exit 1
 rm -rfv ../concord-server/server/src/main/resources/ui/*
 cp -rf build ../concord-server/server/src/main/resources/ui
 
 # Build server
 cd ../concord-server/server
 chmod +x gradlew
-./gradlew build
+./gradlew build || exit 1
 
 project_version=$(gradle properties -q | grep "version:" | awk '{print $2}' | tr -d '[:space:]')
 
 # Package distribution, configuration into zip
-zip ../../distribution-$project_version.zip build/libs/concord-server-$project_version-all.jar src/main/yml/server.yml
+zip ../../distribution-$project_version.zip build/libs/concord-server-$project_version-all.jar src/main/yml/server.yml || exit 1
