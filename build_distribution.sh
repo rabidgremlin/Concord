9dbee0f033a5cbb93d843a5f5d200d5b4f6cbd69
ackej1 <jonathan.ackerman1@airnz.co.nz>
2018-12-11 15:33:02 +1300

    Updated versioning.

--- build_distribution.sh
+++ build_distribution.sh
@@ -20,7 +20,7 @@ echo "Building Server...."
 
 # Package distribution, configuration into zip
 echo "Building distribution zip...."
-project_version=$(gradle properties -q | grep "version:" | awk '{print $2}' | tr -d '[:space:]')
-zip ../../distribution-$project_version.zip build/libs/concord-server-$project_version-all.jar src/main/yml/server.yml || exit 1
+project_version=$(./gradlew properties -q | grep "version:" | awk '{print $2}' | tr -d '[:space:]')
+zip ../../concord-$project_version.zip build/libs/concord-server-$project_version-all.jar src/main/yml/server.yml || exit 1
 
 echo "Done."
