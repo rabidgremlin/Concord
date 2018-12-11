a5da1e26622cccec0e1063a1ce634e64c21a8f59
ackej1 <jonathan.ackerman1@airnz.co.nz>
2018-12-12 12:23:38 +1300

    Enabled UI build.

--- build_distribution.sh
+++ build_distribution.sh
@@ -4,18 +4,18 @@ echo "Building Concord distribution..."
 
 # Build UI
 cd concord-ui
-#echo "Install UI dependencies..."
-#npm install || exit 1
+echo "Install UI dependencies..."
+npm install || exit 1
 
-#echo "Building UI..."
-#npm run build || exit 1
-#rm -rfv ../concord-server/server/src/main/resources/ui/*
-#cp -rf build ../concord-server/server/src/main/resources/ui
+echo "Building UI..."
+npm run build || exit 1
+rm -rfv ../concord-server/server/src/main/resources/ui/*
+cp -rf build ../concord-server/server/src/main/resources/ui
 
 # Build server
+echo "Building Server...."
 cd ../concord-server/server
 chmod +x gradlew
-echo "Building Server...."
 ./gradlew build || exit 1
 
 echo "Done. See concord-server/server/build/distributions"
