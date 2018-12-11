78d005d1e46e4cbe46a12254f5b0e14c2c4bf2ee
ackej1 <jonathan.ackerman1@airnz.co.nz>
2018-12-11 14:16:51 +1300

    Added progress messages.

--- build_distribution.sh
+++ build_distribution.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "Building Concord distribution..."
+
 # Build UI
 cd concord-ui
+echo "Install UI dependencies..."
 npm install || exit 1
+
+echo "Building UI..."
 npm run build || exit 1
 rm -rfv ../concord-server/server/src/main/resources/ui/*
 cp -rf build ../concord-server/server/src/main/resources/ui
@@ -10,9 +15,12 @@ cp -rf build ../concord-server/server/src/main/resources/ui
 # Build server
 cd ../concord-server/server
 chmod +x gradlew
+echo "Building Server...."
 ./gradlew build || exit 1
 
-project_version=$(gradle properties -q | grep "version:" | awk '{print $2}' | tr -d '[:space:]')
-
 # Package distribution, configuration into zip
+echo "Building distribution zip...."
+project_version=$(gradle properties -q | grep "version:" | awk '{print $2}' | tr -d '[:space:]')
 zip ../../distribution-$project_version.zip build/libs/concord-server-$project_version-all.jar src/main/yml/server.yml || exit 1
+
+echo "Done."
