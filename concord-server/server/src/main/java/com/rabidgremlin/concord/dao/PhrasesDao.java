0677574ed623e2b37e417912e91bfb18953c4be2
wilmol <willjoemolloy@gmail.com>
2019-02-28 10:27:43 +1300

    update phrases SQL

--- concord-server/server/src/main/java/com/rabidgremlin/concord/dao/PhrasesDao.java
+++ concord-server/server/src/main/java/com/rabidgremlin/concord/dao/PhrasesDao.java
@@ -13,22 +13,23 @@ import com.rabidgremlin.concord.api.Phrase;
 public interface PhrasesDao
 {
 
-  @SqlUpdate("REPLACE INTO phrases(phraseId, text, completed, label) VALUES (:phraseId, :text,:completed, NULL)")
+  @SqlUpdate("REPLACE INTO phrases(phraseId, text, completed, label, lastModified) VALUES (:phraseId, :text,:completed, NULL, CURRENT_TIMESTAMP)")
   void upsert(@Bind("phraseId") String phraseId, @Bind("text") String text, @Bind("completed") Boolean completed);
 
-  @SqlBatch("REPLACE INTO phrases(phraseId, text, completed) VALUES (:phraseId, :text,:completed)")
+  @SqlBatch("REPLACE INTO phrases(phraseId, text, completed, lastModified) VALUES (:phraseId, :text,:completed, CURRENT_TIMESTAMP)")
   void upsertBatch(@Bind("phraseId") List<String> phraseIds, @Bind("text") List<String> phrases, @Bind("completed") Boolean completed);
+
+  @SqlBatch("update phrases set completed = True, label = :label, completedTimestamp = CURRENT_TIMESTAMP, lastModified = CURRENT_TIMESTAMP where phraseId = :phraseId")
+  void markPhrasesComplete(@Bind("phraseId") List<String> phraseId, @Bind("label") List<String> label);
+
+  @SqlBatch("DELETE from phrases where phrases.phraseId = :phraseId")
+  void deletePhrases(@Bind("phraseId") List<String> phraseId);
 
   @SqlQuery("select p.* from phrases p where p.phraseId not in (select v.phraseId from votes v where v.userId = :userId) AND p.completed = false limit 1")
   @RegisterBeanMapper(Phrase.class)
   Phrase getNextPhraseToLabelForUser(@Bind("userId") String userId);
 
-  @SqlBatch("update phrases set completed = True, label = :label, completedTimestamp = CURRENT_TIMESTAMP where phraseId = :phraseId")
-  void markPhrasesComplete(@Bind("phraseId") List<String> phraseId, @Bind("label") List<String> label);
-
   @SqlQuery("select p.phraseId from phrases p where p.completed = True")
   List<String> getCompletedPhraseIdentifiers();
 
-  @SqlBatch("DELETE from phrases where phrases.phraseId = :phraseId")
-  void deleteCompletedPhrases(@Bind("phraseId") List<String> phraseId);
 }
