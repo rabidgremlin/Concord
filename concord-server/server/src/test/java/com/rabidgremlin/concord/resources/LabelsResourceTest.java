335a3744a8d56a297acad07aba15cc7870aceebd
rabidgremlin <jack@rabidgremlin.com>
2019-03-01 17:56:35 +1300

    WIP.

--- concord-server/server/src/test/java/com/rabidgremlin/concord/resources/LabelsResourceTest.java
+++ concord-server/server/src/test/java/com/rabidgremlin/concord/resources/LabelsResourceTest.java
@@ -3,7 +3,6 @@ package com.rabidgremlin.concord.resources;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
@@ -18,58 +17,94 @@ import org.mockito.MockitoAnnotations;
 
 import com.rabidgremlin.concord.api.Label;
 import com.rabidgremlin.concord.auth.Caller;
-import com.rabidgremlin.concord.dao.LabelsDao;
 
 public class LabelsResourceTest
+    extends BaseResourceTest
 {
   private LabelsResource labelsResource;
-
-  private List<Label> labels = new ArrayList<>();
 
   @Mock
   private Caller callerMock;
 
-  @Mock
-  private LabelsDao labelsDaoMock;
-
   @Before
   public void setUp()
+    throws Exception
   {
     MockitoAnnotations.initMocks(this);
-    labelsResource = new LabelsResource(labelsDaoMock);
 
-    Label label1 = new Label();
-    label1.setLabel("OrderTaxi");
-    label1.setShortDescription("Order Taxi");
-    label1.setLongDescription("Where would you like your taxi to be sent ?");
+    setUpDatabase();
 
-    labels.add(label1);
+    labelsResource = createLabelsResource();
   }
 
   @Test
   public void canUploadCSVOfLabels()
   {
+    // given this list of labels
+    List<Label> labels = new ArrayList<>();
+    Label label1 = new Label();
+    label1.setLabel("OrderTaxi");
+    label1.setShortDescription("Order Taxi");
+    label1.setLongDescription("Where would you like your taxi to be sent ?");
+    labels.add(label1);
+
+    // when we call upload csv with list of labels
     Response response = labelsResource.uploadCsv(callerMock, labels);
 
-    verify(labelsDaoMock, times(1)).upsert(anyList());
+    // then it is successful
     assertThat(response, instanceOf(Response.class));
     assertEquals(200, response.getStatus());
     assertEquals("OK", response.getStatusInfo().toString());
+
+    // then the list of labels are stored (should we do this check with a SQL query?)
+    response = labelsResource.getLabels(callerMock);
+    assertThat(response, instanceOf(Response.class));
+    assertEquals(200, response.getStatus());
+    assertEquals("OK", response.getStatusInfo().toString());
+    assertEquals(response.getEntity(), labels);
   }
 
   @Test
   public void canReturnAllLabels()
   {
+    // given this list of labels stored in the database
+    List<Label> labels = new ArrayList<>();
+    Label label1 = new Label();
+    label1.setLabel("OrderTaxi");
+    label1.setShortDescription("Order Taxi");
+    label1.setLongDescription("Where would you like your taxi to be sent ?");
+    labels.add(label1);
     labels.add(new Label("CancelTaxi", "Cancel Taxi", "Cancel my taxi"));
     labels.add(new Label("WhereTaxi", "Where taxi", "Where is my taxi"));
+    Response response = labelsResource.uploadCsv(callerMock, labels);
+    assertThat(response, instanceOf(Response.class));
+    assertEquals(200, response.getStatus());
 
-    when(labelsDaoMock.getLabels()).thenReturn(labels);
+    // when we retrieve the list of labels
 
-    Response response = labelsResource.getLabels(callerMock);
+    response = labelsResource.getLabels(callerMock);
 
+    // then we get backthe list of labels we expect
     assertThat(response, instanceOf(Response.class));
     assertEquals(200, response.getStatus());
     assertEquals("OK", response.getStatusInfo().toString());
     assertEquals(response.getEntity(), labels);
+  }
+
+  @Test
+  public void canAddASingleLabel()
+  {
+    Caller caller = new Caller("bob");
+    Label label = new Label();
+    label.setLabel("OrderTaxi");
+    label.setShortDescription("Order Taxi");
+    label.setLongDescription("Where would you like your taxi to be sent ?");
+
+    Response response = labelsResource.upsert(caller, label);
+
+    assertThat(response, instanceOf(Response.class));
+    assertEquals(201, response.getStatus());
+
+    // TODO check that record is in database
   }
 }
