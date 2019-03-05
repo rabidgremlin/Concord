d73d064c1fa9c2304a87e6bef7d8f9a85e760957
wilmol <willjoemolloy@gmail.com>
2019-03-05 15:54:02 +1300

    populating data table

--- concord-ui/src/components/statstable.js
+++ concord-ui/src/components/statstable.js
@@ -1,53 +1,89 @@
 import React, {Component} from 'react';
 
-import {DataTable, DataTableContent, DataTableHead, DataTableRow, DataTableHeadCell, DataTableBody, DataTableCell} from 'rmwc/DataTable';
+import {
+    DataTable,
+    DataTableBody,
+    DataTableCell,
+    DataTableContent,
+    DataTableHead,
+    DataTableHeadCell,
+    DataTableRow
+} from 'rmwc/DataTable';
 import '@rmwc/data-table/data-table.css';
 
 export default class StatsTable extends Component {
 
-    render() {
-        console.log("Rendering stats");
+    constructor(props) {
+        super(props);
+        this.state = ({})
+    }
+
+
+    componentWillMount() {
+        console.log("Fetching stats");
         fetch('/api/stats')
             .then(results => results.json())
-            .then(data => {
-                data.forEach(console.log);
+            .then(results => {
+                this.setState({
+                    data: results
+                })
             });
-        return (
-            <DataTable
-                style={{ height: '300px', width: '375px' }}
-            >
-                <DataTableContent>
-                    <DataTableHead>
-                        <DataTableRow>
-                            <DataTableHeadCell>Items</DataTableHeadCell>
-                            <DataTableHeadCell>
-                                Quantity
-                            </DataTableHeadCell>
-                            <DataTableHeadCell alignEnd>
-                                Unit price
-                            </DataTableHeadCell>
-                        </DataTableRow>
-                    </DataTableHead>
-                    <DataTableBody>
-                        <DataTableRow>
-                            <DataTableCell>Cookies</DataTableCell>
-                            <DataTableCell alignEnd>25</DataTableCell>
-                            <DataTableCell alignEnd>$2.90</DataTableCell>
-                        </DataTableRow>
-                        <DataTableRow activated>
-                            <DataTableCell>Pizza</DataTableCell>
-                            <DataTableCell alignEnd>50</DataTableCell>
-                            <DataTableCell alignEnd>$1.25</DataTableCell>
-                        </DataTableRow>
-                        <DataTableRow>
-                            <DataTableCell>Icecream</DataTableCell>
-                            <DataTableCell alignEnd>10</DataTableCell>
-                            <DataTableCell alignEnd>$2.35</DataTableCell>
-                        </DataTableRow>
-                    </DataTableBody>
-                </DataTableContent>
-            </DataTable>
-        )
+    }
+
+    render() {
+        const data = this.state.data;
+        console.log(data);
+        if (data) {
+            console.log("Rendering stats");
+            data.forEach(console.log);
+            console.log(data[0].userId);
+            return (
+                <DataTable
+                    style={{height: window.innerHeight, width: window.innerWidth}}
+                >
+                    <DataTableContent>
+                        <DataTableHead>
+                            <DataTableRow>
+                                <DataTableHeadCell>
+                                    User
+                                </DataTableHeadCell>
+                                <DataTableHeadCell alignEnd>
+                                    Total Phrases
+                                </DataTableHeadCell>
+                                <DataTableHeadCell alignEnd>
+                                    Completed Phrases
+                                </DataTableHeadCell>
+                                <DataTableHeadCell alignEnd>
+                                    Trashed Phrases
+                                </DataTableHeadCell>
+                                <DataTableHeadCell alignEnd>
+                                    Completed Success Rate
+                                </DataTableHeadCell>
+                                <DataTableHeadCell alignEnd>
+                                    Trashed Rate
+                                </DataTableHeadCell>
+                            </DataTableRow>
+                        </DataTableHead>
+                        <DataTableBody>
+                            {[...Array(data.length)].map((v, i) => (
+                                <DataTableRow key={i}>
+                                    <DataTableCell>{data[i].userId}</DataTableCell>
+                                    <DataTableCell alignEnd>{data[i].totalVotes}</DataTableCell>
+                                    <DataTableCell alignEnd>{data[i].completedVotes}</DataTableCell>
+                                    <DataTableCell alignEnd>{data[i].trashVotes}</DataTableCell>
+                                    <DataTableCell alignEnd>{(100 * data[i].completedSuccessRatio).toFixed(0)}%</DataTableCell>
+                                    <DataTableCell alignEnd>{(100 * data[i].trashRatio).toFixed(0)}%</DataTableCell>
+                                </DataTableRow>
+                            ))}
+                        </DataTableBody>
+                    </DataTableContent>
+                </DataTable>
+            )
+        } else {
+            return (
+                <div><p>Loading...</p></div>
+            )
+        }
     }
 
 }
