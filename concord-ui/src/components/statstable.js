aec7e9b0b0ae80d69e9ede024fb574ff32fb8b8c
wilmol <willjoemolloy@gmail.com>
2019-03-05 15:24:01 +1300

    upgrade RMWC to v4.0.6 - WORKS
    - bug in version 5 ?? they dont work

--- concord-ui/src/components/statstable.js
+++ concord-ui/src/components/statstable.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 
+import {DataTable, DataTableContent, DataTableHead, DataTableRow, DataTableHeadCell, DataTableBody, DataTableCell} from 'rmwc/DataTable';
+import '@rmwc/data-table/data-table.css';
+
 export default class StatsTable extends Component {
 
     render() {
@@ -10,7 +13,40 @@ export default class StatsTable extends Component {
                 data.forEach(console.log);
             });
         return (
-            <div>STATS</div>
+            <DataTable
+                style={{ height: '300px', width: '375px' }}
+            >
+                <DataTableContent>
+                    <DataTableHead>
+                        <DataTableRow>
+                            <DataTableHeadCell>Items</DataTableHeadCell>
+                            <DataTableHeadCell>
+                                Quantity
+                            </DataTableHeadCell>
+                            <DataTableHeadCell alignEnd>
+                                Unit price
+                            </DataTableHeadCell>
+                        </DataTableRow>
+                    </DataTableHead>
+                    <DataTableBody>
+                        <DataTableRow>
+                            <DataTableCell>Cookies</DataTableCell>
+                            <DataTableCell alignEnd>25</DataTableCell>
+                            <DataTableCell alignEnd>$2.90</DataTableCell>
+                        </DataTableRow>
+                        <DataTableRow activated>
+                            <DataTableCell>Pizza</DataTableCell>
+                            <DataTableCell alignEnd>50</DataTableCell>
+                            <DataTableCell alignEnd>$1.25</DataTableCell>
+                        </DataTableRow>
+                        <DataTableRow>
+                            <DataTableCell>Icecream</DataTableCell>
+                            <DataTableCell alignEnd>10</DataTableCell>
+                            <DataTableCell alignEnd>$2.35</DataTableCell>
+                        </DataTableRow>
+                    </DataTableBody>
+                </DataTableContent>
+            </DataTable>
         )
     }
 
