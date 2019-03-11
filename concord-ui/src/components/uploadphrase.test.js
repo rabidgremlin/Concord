8f26761a37e59c4544338e62380a78937cc670e6
wilmol <willjoemolloy@gmail.com>
2019-03-12 12:23:14 +1300

    add unit tests

---
+++ concord-ui/src/components/uploadphrase.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {
+  DataTable,
+  DataTableBody,
+  DataTableCell,
+  DataTableContent,
+  DataTableHead,
+  DataTableHeadCell,
+  DataTableRow
+} from 'rmwc/DataTable';
+import { UploadPhrase } from './uploadphrase';
+import { Dialog, DialogActions, DialogButton, DialogContent, DialogTitle } from '@rmwc/dialog';
+import { Button, TextField } from 'rmwc';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+  const props = {
+    dispatch: jest.fn()
+  };
+  const uploadPhrase = shallow(<UploadPhrase {...props} />);
+  console.log(uploadPhrase.debug());
+  return {
+    props,
+    uploadPhrase
+  };
+}
+
+describe('UploadPhrase', () => {
+  it('renders without crashing', () => {
+    const { uploadPhrase, props } = setup();
+    expect(uploadPhrase.find(UploadPhrase));
+
+    expect(uploadPhrase.find(UploadPhrase).containsMatchingElement(TextField));
+
+    expect(uploadPhrase.find(UploadPhrase).containsMatchingElement(Button));
+
+    expect(uploadPhrase.find(UploadPhrase).containsMatchingElement(DataTable));
+    expect(uploadPhrase.find(DataTable).containsMatchingElement(DataTableContent));
+    expect(uploadPhrase.find(DataTableContent).containsMatchingElement(DataTableHead));
+    expect(uploadPhrase.find(DataTableContent).containsMatchingElement(DataTableBody));
+    expect(uploadPhrase.find(DataTableBody).containsMatchingElement(DataTableRow));
+    expect(uploadPhrase.find(DataTableRow).containsMatchingElement(DataTableCell));
+    expect(uploadPhrase.find(DataTableRow).containsMatchingElement(DataTableHeadCell));
+
+    expect(uploadPhrase.find(UploadPhrase).containsMatchingElement(Dialog));
+    expect(uploadPhrase.find(Dialog).containsMatchingElement(DialogTitle));
+    expect(uploadPhrase.find(Dialog).containsMatchingElement(DialogContent));
+    expect(uploadPhrase.find(Dialog).containsMatchingElement(DialogActions));
+    expect(uploadPhrase.find(DialogActions).containsMatchingElement(DialogButton));
+  });
+});
